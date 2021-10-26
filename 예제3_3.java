45e9418860072c6c205727a3d89f39d780eef7b2
mohakkayo <93104200+mohakkayo@users.noreply.github.com>
2021-10-26 14:46:35 +0900

    Add files via upload

---
+++ 예제3_3.java
@@ -0,0 +1,15 @@
+
+public class ¿¹Á¦3_3 {
+
+	public static void main(String[] args) {
+		char c= 'a';
+		
+		do {
+			System.out.print(c);
+			c= (char) (c+1);
+		} while (c <= 'z');
+	
+		}
+
+	}
+
